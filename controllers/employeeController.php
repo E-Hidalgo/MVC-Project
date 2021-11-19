693eced4be492f9505827e8097a6bedcb21e2c65
Enrique Hidalgo <koko.freelance@gmail.com>
2021-11-19 19:30:16 +0000

    getAllEmployees working OK

--- controllers/employeeController.php
+++ controllers/employeeController.php
@@ -1,2 +1,20 @@
 <?php
-echo "qui fuennnnn";
+
+require_once MODELS . "employeeModel.php";
+
+$action= "";
+
+if(isset($_GET["action"])) {
+$action= $_GET["action"];
+}
+
+if(function_exists($action)) {
+  echo "si existe";
+  call_user_func($action);
+} else {
+  echo "no existe";
+}
+
+function getAllEmployees(){
+  echo "dentro de la función";
+};

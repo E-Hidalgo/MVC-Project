aa1c0692a1a0c50b7aa25e7cb747aecc1287a2e0
Enrique Hidalgo <koko.freelance@gmail.com>
2021-11-19 18:22:58 +0000

    employeeController redirecting OK

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 
-require_once ("./config/baseConstants.php");
-require_once ("./config/db.php");
+require_once ("config/constants.php");
+require_once ("config/db.php");
 
 if(isset($_GET["controller"])) {
-  echo "Hey";
+$controller = getControllerPath($_GET["controller"]);
+$fileExists = file_exists($controller);
+
+if($fileExists) {
+require_once ($controller);
+} else {
+$errorMsg= "The page you are trying to access doesn't exist!";
+require_once VIEWS . "error/error.php";
+};
 }
+
+// This returns the controller path dinamically.
+function getControllerPath($controller):string {
+return CONTROLLERS .$controller."Controller.php";
+}
+
+?>
